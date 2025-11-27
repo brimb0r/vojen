7c7649637c6807d494548c632ee78fd1c42cacfb
brimb0r <gagnonc0713@outlook.com>
2025-11-27 17:02:52 -0500

    stamina regen when swimming

--- mods/regen.cs
+++ mods/regen.cs
@@ -1,6 +1,8 @@
 using BepInEx;
 using HarmonyLib;
 using BepInEx.Configuration;
+using UnityEngine;
+using System;
 
 namespace vojen.mods
 {
@@ -21,6 +23,18 @@ namespace vojen.mods
         void OnDestroy()
         {
             harmony.UnpatchSelf();
+        }
+        
+        [HarmonyPatch(typeof(Player), "UpdateStats", typeof(float))]
+        static class Player_UpdateStats_Patch
+        {
+            static void Postfix(float dt, Character __instance, ref float ___m_stamina)
+            {
+                if (__instance.IsSwimming() && !__instance.IsOnGround())
+                {
+                    ___m_stamina = Mathf.Min(__instance.GetMaxStamina(), ___m_stamina + TreadingStaminaRegen.Value * dt);
+                }
+            }
         }
     }
 }
